f017e8ccf038ee74b37fbba5b9362fea1a8de00c
camerontsang <camerontsang@gmail.com>
2025-07-13 08:59:44 -0400

    final: fixed background

--- src/components/MovingBackground.jsx
+++ src/components/MovingBackground.jsx
@@ -4,37 +4,28 @@ import TextBackground from './TextBackground';
 const MovingBackground = ({ show = true }) => {
   if (!show) return null;
 
+  const layerStyle = {
+    position: 'absolute',
+    width: '100vw',
+    height: '120px',
+    left: 0,
+  };
+
   return (
-    <div className="fixed inset-0 z-0 overflow-hidden pointer-events-none">
-      {/* Layer 1 - Moving Left */}
-      <div className="absolute w-full h-32" style={{ top: '14.28%' }}>
-        <TextBackground
-          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
-          velocity={-25}
-          className="text-gray-300 opacity-15"
-        />
-      </div>
-      
-      {/* Layer 2 - Moving Right */}
-      <div className="absolute w-full h-32" style={{ top: '28.56%' }}>
-        <TextBackground
-          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
-          velocity={25}
-          className="text-gray-300 opacity-15"
-        />
-      </div>
-      
-      {/* Layer 3 - Moving Left */}
-      <div className="absolute w-full h-32" style={{ top: '42.84%' }}>
-        <TextBackground
-          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
-          velocity={-25}
-          className="text-gray-300 opacity-15"
-        />
-      </div>
-      
-      {/* Layer 4 - Moving Right */}
-      <div className="absolute w-full h-32" style={{ top: '57.12%' }}>
+    <div 
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        zIndex: 0,
+        overflow: 'hidden',
+        pointerEvents: 'none'
+      }}
+    >
+      {/* Layer 1 - Moving Right */}
+      <div style={{ ...layerStyle, top: '-20px' }}>
         <TextBackground
           texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
           velocity={25}
@@ -42,8 +33,44 @@ const MovingBackground = ({ show = true }) => {
         />
       </div>
       
-      {/* Layer 5 - Moving Left */}
-      <div className="absolute w-full h-32" style={{ top: '71.4%' }}>
+      {/* Layer 2 - Moving Left */}
+      <div style={{ ...layerStyle, top: '100px' }}>
+        <TextBackground
+          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
+          velocity={-25}
+          className="text-gray-300 opacity-15"
+        />
+      </div>
+      
+      {/* Layer 3 - Moving Right */}
+      <div style={{ ...layerStyle, top: '220px' }}>
+        <TextBackground
+          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
+          velocity={25}
+          className="text-gray-300 opacity-15"
+        />
+      </div>
+      
+      {/* Layer 4 - Moving Left */}
+      <div style={{ ...layerStyle, top: '340px' }}>
+        <TextBackground
+          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
+          velocity={-25}
+          className="text-gray-300 opacity-15"
+        />
+      </div>
+      
+      {/* Layer 5 - Moving Right */}
+      <div style={{ ...layerStyle, top: '460px' }}>
+        <TextBackground
+          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
+          velocity={25}
+          className="text-gray-300 opacity-15"
+        />
+      </div>
+      
+      {/* Layer 6 - Moving Left */}
+      <div style={{ ...layerStyle, top: '580px' }}>
         <TextBackground
           texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
           velocity={-20}
@@ -51,11 +78,20 @@ const MovingBackground = ({ show = true }) => {
         />
       </div>
       
-      {/* Layer 6 - Moving Right */}
-      <div className="absolute w-full h-32" style={{ top: '85.68%' }}>
+      {/* Layer 7 - Moving Right */}
+      <div style={{ ...layerStyle, top: '700px' }}>
         <TextBackground
           texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
           velocity={25}
+          className="text-gray-300 opacity-15"
+        />
+      </div>
+      
+      {/* Layer 8 - Moving Left */}
+      <div style={{ ...layerStyle, top: '820px' }}>
+        <TextBackground
+          texts={['Cameron Tsang - Data Science & Math @ NYU Shanghai']}
+          velocity={-25}
           className="text-gray-300 opacity-15"
         />
       </div>
